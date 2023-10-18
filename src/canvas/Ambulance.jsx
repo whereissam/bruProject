16fa11338658d2d90b48cdba750de630d51fd799
whereissam <sam135642@gmail.com>
2023-10-18 14:16:05 +0800

    style: change aubulance position

--- src/canvas/Ambulance.jsx
+++ src/canvas/Ambulance.jsx
@@ -1,14 +1,25 @@
 import { useLoader } from '@react-three/fiber'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
-import React from 'react'
+import React, { useEffect, useState  } from 'react'
 
 const Ambulance = () => {
   const model = useLoader(GLTFLoader, './ambulance.glb')
-    console.log(model)
+  console.log(model)
+
+  const [rotation, setRotation] = useState([0, 0, 0]) // Initial rotation (in radians)
+
+  useEffect(() => {
+    const handleRotation = () => {
+      // Rotate the model 45 degrees around the Y-axis each time this function is called
+      setRotation([6, rotation[1] - Math.PI / 4, 0])
+    }
+
+    handleRotation()
+  }, [])
 
   return (
     <group>
-        <mesh>
+        <mesh rotation={rotation}>
             <primitive object={ model.scene } scale={ 3 } />
         </mesh>
     </group>
